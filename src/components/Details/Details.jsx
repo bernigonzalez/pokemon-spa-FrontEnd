af38d60f7b6b28c5f05751c2edc8c7e4ce6f966b
Bernardo Gonzalez <60318571+bernigonzalez@users.noreply.github.com>
2022-03-29 20:50:50 -0300

    fix bug detail pokemon
    fix bug detail pokemon

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -29,40 +29,40 @@ export default function Detail(props){
                 <br />
                 <br />
                 <div className={styles.card}>
-                <div className={styles.title}>{pokemon[0].name.charAt(0).toUpperCase() + pokemon[0].name.slice(1)}</div>
+                <div className={styles.title}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</div>
                 <div>
                     <img className={styles.image} src={pokemon[0].image} alt={pokemon[0].name} />
                 </div>
                 <div>#{pokemon[0].id}</div>
                  <div>
-                    <div className={styles.types}>Type 1: {(!pokemon[0].createdInDb)  ? pokemon[0].types[0] : pokemon[0].types[0].name}</div>
-                    <div className={styles.types}>Type 2: {(!pokemon[0].createdInDb)  ? pokemon[0].types[1] : ((pokemon[0].types[1] ? pokemon[0].types[1].name : " "))}</div> 
+                    <div className={styles.types}>Type 1: {(!pokemon.createdInDb)  ? pokemon.types[0] : pokemon.types[0].name}</div>
+                    <div className={styles.types}>Type 2: {(!pokemon.createdInDb)  ? pokemon.types[1] : ((pokemon.types[1] ? pokemon.types[1].name : " "))}</div> 
                     
                 </div>
                 <div>
                     <div className={styles.stats}>
                         <div>HP</div>
-                        <div>{pokemon[0].hp}</div>
+                        <div>{pokemon.hp}</div>
                     </div>
                     <div className={styles.stats}>
                         <div>Attack</div>
-                        <div>{pokemon[0].attack}</div>
+                        <div>{pokemon.attack}</div>
                     </div>
                     <div className={styles.stats}>
                         <div>Defense</div>
-                        <div>{pokemon[0].defense}</div>
+                        <div>{pokemon.defense}</div>
                     </div>
                     <div className={styles.stats}>
                         <div>Speed</div>
-                        <div>{pokemon[0].speed}</div>
+                        <div>{pokemon.speed}</div>
                     </div>
                     <div className={styles.stats}>
                         <div>Weight</div>
-                        <div>{pokemon[0].weight}</div>
+                        <div>{pokemon.weight}</div>
                     </div>
                     <div className={styles.stats}>
                         <div>Height</div>
-                        <div>{pokemon[0].height}</div>
+                        <div>{pokemon.height}</div>
                     </div>   
                 </div>
 
